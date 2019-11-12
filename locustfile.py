8afdfc727ac6072cc9090a21c8f183307886fc90
King.Zevin <king.zevin@gmail.com>
2019-11-12 23:46:22 +0800

    [locust+project] added the ENTER PRINT for each function with print_stack

--- locustfile.py
+++ locustfile.py
@@ -24,9 +24,11 @@ from loadgenerator import project, csrf
 import metrics
 import logparser
 ## zevin
+import traceback
 from inspect import currentframe, getframeinfo
 def LINE():
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     return "LINE:" + str(getframeinfo(currentframe()).filename) + ":" + str(getframeinfo(currentframe()).lineno)
     getframeinfo(currentframe()).code_context
 ##
@@ -54,11 +56,13 @@ WEB_LOGS_PATH           = os.environ.get("LOCUST_LOG_PATH", "logs") # path to na
 
 def wait(self):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     gevent.sleep(random.normalvariate(WAIT_MEAN, WAIT_STD))
 TaskSet.wait = wait
 
 def login(l):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     resp = l.client.get("/login")
     l.csrf_token = csrf.find_in_page(resp.content)
     data = {
@@ -71,6 +75,7 @@ def login(l):
 
 def create_delete_project(l):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     d = {"_csrf": l.csrf_token, "projectName": "123", "template": None}
     r = l.client.post("/project/new", json=d)
     l.client.delete("/project/%s" % r.json()["project_id"],
@@ -79,26 +84,31 @@ def create_delete_project(l):
 
 def settings(l):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     l.client.get("/user/settings")
     d = dict(_csrf=l.csrf_token, email=l.parent.email, first_name="foo", last_name="bar")
     assert l.client.post("/user/settings", json=d).text == "OK"
 
 def stop(l):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     l.interrupt()
 
 def register(l):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     l.client.get("/register")
 
 def index(l):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     l.client.get("/")
 
 class ProjectOverview(TaskSet):
     tasks = { project.Page: 30, create_delete_project: 2, stop: 1, settings: 1 }
     def on_start(self):
         print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+        traceback.print_stack()
         r = self.client.get("/project")
         projects = re.search("\"projects\":(\\[[^\\]]*\\])", r.content.decode("utf-8") , re.MULTILINE)
         projects = re.search("\"projects\":(\\[[^\\]]*\\])", r.content.decode("utf-8") , re.MULTILINE).group(1)
@@ -112,6 +122,7 @@ class UserBehavior(TaskSet):
     tasks = {ProjectOverview: 10, register: 1, index: 1}
     def on_start(self):
         print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+        traceback.print_stack()
         global user
         global logins_per_acc
         user += 1.0 / logins_per_acc
@@ -123,6 +134,7 @@ class WebsiteUser(HttpLocust):
     if LOAD_TYPE == "nasa" or LOAD_TYPE == "worldcup":
         def __init__(self, client_id, timestamps, queue):
             print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+            traceback.print_stack()
             self.request_timestamps = timestamps
             self.request_number = 1
             self.client_id = client_id
@@ -138,6 +150,7 @@ class RequestStats():
 
     def requests_success(self, request_type="", name="", response_time=0, **kw):
         print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+        traceback.print_stack()
         STATSD.timing(request_type + "-" + name, response_time)
         if not request_type.startswith("WebSocket"):
             print "LINE:" + str(getframeinfo(currentframe()).filename) + ":" + str(getframeinfo(currentframe()).lineno) + "," + str("%s - %s: %s" % (request_type, name, response_time))
@@ -145,6 +158,7 @@ class RequestStats():
 
     def requests_failure(self, request_type="", name="", response_time=0, exception=None, **kw):
         print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+        traceback.print_stack()
         STATSD.timing(request_type + "-" + name + "-error", response_time)
         if not request_type.startswith("WebSocket"):
             print "LINE:" + str(getframeinfo(currentframe()).filename) + ":" + str(getframeinfo(currentframe()).lineno) + "," + str("%s - %s: %s" % (request_type, name, response_time))
@@ -152,11 +166,13 @@ class RequestStats():
 
     def locust_error(self, locust_instance=None, exception=None, tb=None):
         print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+        traceback.print_stack()
         STATSD.incr(locust_instance.__class__.__name__ + "-" + exception.__class__.__name__)
         STATSD.incr("requests_error")
 
 def stop_measure(started_at):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     ended_at = datetime.utcnow()
     metadata = {}
     for k, v in os.environ.items():
@@ -171,6 +187,7 @@ def stop_measure(started_at):
 
 def constant_measure(*args, **kw):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     # wait for the load generator to take effect
     time.sleep(10)
     started_at = datetime.utcnow()
@@ -179,16 +196,19 @@ def constant_measure(*args, **kw):
 
 def start_hatch(users, hatch_rate):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     payload = dict(locust_count=users, hatch_rate=hatch_rate)
     r = requests.post("http://localhost:8089/swarm", data=payload)
     print "LINE:" + str(getframeinfo(currentframe()).filename) + ":" + str(getframeinfo(currentframe()).lineno) + "," + str(r.text)
 
 def print_color(text):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     print "LINE:" + str(getframeinfo(currentframe()).filename) + ":" + str(getframeinfo(currentframe()).lineno) + "," + str("\x1B[31;40m%s\x1B[0m" % text)
 
 def process_requests(self):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     i = self.locust.request_number
     timestamps = self.locust.request_timestamps
     if i < timestamps.size:
@@ -207,6 +227,7 @@ def process_requests(self):
 
 def report_users():
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     while True:
         try:
             val = runners.locust_runner.user_count
@@ -218,6 +239,7 @@ def report_users():
 GREENLETS = []
 def replay_log_measure(df):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     TaskSet.wait = process_requests
     runner = runners.locust_runner
     locust = runner.locust_classes[0]
@@ -251,6 +273,7 @@ def replay_log_measure(df):
 
 def random_measure():
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     runner = runners.locust_runner
     locust = runner.locust_classes[0]
     def start_locust(_):
@@ -301,6 +324,7 @@ def random_measure():
 
 def read_log(type):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     if type == "nasa":
         read_log = logparser.read_nasa
     else: # "worldcup"
@@ -315,6 +339,7 @@ def read_log(type):
 
 def session_number(v):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     diff = v.timestamp.diff(1)
     diff.fillna(0, inplace=True)
     sessions = (diff > pd.Timedelta(minutes=10)).cumsum()
@@ -324,12 +349,14 @@ def session_number(v):
 
 def started_at(v):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     data = dict(client_id=v.client_id, timestamp=v.timestamp, session_id=v.session_id,
                 started_at=[v.timestamp.iloc[0]] * len(v.timestamp))
     return pd.DataFrame(data)
 
 def group_log_by_sessions(df):
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     df = df.sort_values("timestamp")
     per_client = df.groupby(df.client_id, sort=False)
     with_session = per_client.apply(session_number)
@@ -338,6 +365,7 @@ def group_log_by_sessions(df):
 
 def measure():
     print "Enter:" + str(getframeinfo(currentframe()).filename + ":" + getframeinfo(currentframe()).function) + "-LINE:" + str(getframeinfo(currentframe()).lineno) + ",code_context:"+ str(getframeinfo(currentframe()).code_context)
+    traceback.print_stack()
     RequestStats() # 初始化events.request_success, events.request_failure, events.locust_error
     time.sleep(5) # 为什么?
     if LOAD_TYPE == "constant":
